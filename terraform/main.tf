69016d767c2623fc2cf19579edef4c27e002bf1d
vvp310 <vvp310@gmail.com>
2020-08-10 10:01:20 +0000

    Create terrafirm main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+terraform {  
+  # Версия terraform  
+  required_version = "0.11.14"
+}
+
+provider "google" {  
+  # Версияпровайдера  version = "2.0.0"  
+  # ID проекта  
+  project = "project-44470"
+  region = "europ44470 -west-1"
+}
